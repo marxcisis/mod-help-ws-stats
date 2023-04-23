75db987e813e47bf8a562d8cb91500a5fe0f5b11
Māris Reinolds <marisreinolds@gmail.com>
2023-04-23 18:21:13 +0300

    fix

--- index.php
+++ index.php
@@ -740,29 +740,10 @@ mod-QdmFm6WcJGj - initializes the factions relations from the map settings
 						</p>
 					</section>
 				</article>
-
-				<footer class="footer">
-					<div class="container text-center py-5">
-						<!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
-						<small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"
-								style="color: #fb866a"></i> by MarkoLV
-							for developers</small>
-					</div>
-				</footer>
+				<?php require_once('parts/footer.php');?>
 			</div>
 		</div>
 	</div>
-	<!--//docs-wrapper-->
-
-	<!-- Javascript -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-
-	<!-- Page Specific JS -->
-	<script src="assets/plugins/smoothscroll.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.8/highlight.min.js"></script>
-	<script src="assets/js/highlight-custom.js"></script>
-	<script src="assets/plugins/gumshoe/gumshoe.polyfills.min.js"></script>
-	<script src="assets/js/docs.js"></script>
+	<?php require_once('parts/foot.php');?>
 </body>
-
 </html>
---
+++ parts/foot.php
@@ -0,0 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="assets/plugins/smoothscroll.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.8/highlight.min.js"></script>
+<script src="assets/js/highlight-custom.js"></script>
+<script src="assets/plugins/gumshoe/gumshoe.polyfills.min.js"></script>
+<script src="assets/js/docs.js"></script>
